aaca0e894718207c2001b8e9617c3796c88f0375
Alexandre Ferrreira <lexanjos@icloud.com>
2025-05-10 17:26:44 +0100

    Refactor CreaturesController to use CreatureRepository.
    This update removes direct database interactions from the controller and delegates them to a new CreatureRepository for better separation of concerns. It also introduces the use of HttpStatusCodes for improved readability and consistency in HTTP responses.

--- src/Controller/CreaturesController.php
+++ src/Controller/CreaturesController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Repository\CreatureRepository;
+use App\Util\HttpStatusCodes;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
 use App\Dto\CreatureListParameterDto;
@@ -9,7 +11,15 @@ use Database\Database;
 
 class CreaturesController 
 {
-  public function listCreatures(Request $request, Response $response): Response
+    private $creatureRepository;
+    public function __construct() {
+        $this->creatureRepository = new CreatureRepository();
+    }
+
+    /**
+     * @throws \Exception
+     */
+    public function listCreatures(Request $request, Response $response): Response
   {
     $queryParams = $request->getQueryParams();
     $body = $response->getBody();
@@ -17,64 +27,37 @@ class CreaturesController
       $params = new CreatureListParameterDto($queryParams);
     } catch (\InvalidArgumentException $e) {
       $body->write(json_encode(['error' => $e->getMessage()]));
-      return $response->withStatus(400)->withHeader('Content-Type', 'application/json')->withBody($body);
+      return $response->withStatus(HttpStatusCodes::BAD_REQUEST)->withHeader('Content-Type', 'application/json')->withBody($body);
     }
 
-    $db = new Database();
-
-    $query = "SELECT * FROM creatures ORDER BY {$params->sortBy} {$params->sortOrder} LIMIT {$params->limit} OFFSET {$params->offset}";
-    $creatures = $db->pdo->query($query)->fetchAll();
+    $creatures = $this->creatureRepository->getCreatures($params);
 
     if(empty($creatures)) {
       $body->write(json_encode(['error' => 'No creatures found']));
-      return $response->withStatus(404)->withHeader('Content-Type', 'application/json')->withBody($body);
+      return $response->withStatus(HttpStatusCodes::OK)->withHeader('Content-Type', 'application/json')->withBody($body);
     }
 
     $body->write(json_encode($creatures));
-    return $response->withStatus(200)->withHeader('Content-Type', 'application/json')->withBody($body);
+    return $response->withStatus(HttpStatusCodes::OK)->withHeader('Content-Type', 'application/json')->withBody($body);
   }
 
   public function createCreature(Request $request, Response $response): Response {
     $parsedBody = $request->getParsedBody();
-    $query = 'INSERT INTO creatures (name, type, habitat, danger_level, created_at, updated_at) VALUES (:name, :type, :habitat, :danger_level, :created_at, :updated_at)';
-    $db = new Database();
-    $stmt = $db->pdo->prepare($query);
-    $stmt->bindParam(':name', $parsedBody['name']);
-    $stmt->bindParam(':type', $parsedBody['type']);
-    $stmt->bindParam(':habitat', $parsedBody['habitat']);
-    $stmt->bindParam(':danger_level', $parsedBody['danger_level']);
-    $stmt->bindParam(':created_at', $parsedBody['created_at']);
-    $stmt->bindParam(':updated_at', $parsedBody['updated_at']);
-    $stmt->execute();
-    $id = $db->pdo->lastInsertId();
-    $query = 'SELECT * FROM creatures WHERE id = :id';
-    $stmt = $db->pdo->prepare($query);
-    $stmt->bindParam(':id', $id);
-    $stmt->execute();
-    $creature = $stmt->fetch();
+    $this->creatureRepository->create($parsedBody);
+    $creature = $this->creatureRepository->getLastById();
     $body = $response->getBody();
     $body->write(json_encode($creature));
-    return $response->withStatus(201)->withHeader('Content-Type', 'application/json')->withBody($body);
+    return $response->withStatus(HttpStatusCodes::CREATED)->withHeader('Content-Type', 'application/json')->withBody($body);
   }
 
   public function updateCreature(Request $request, Response $response): Response {
     $parsedBody = $request->getParsedBody();
     $id = $request->getAttribute('id');
-    // $query = 'UPDATE creatures SET name = :name, type = :type, habitat = :habitat, danger_level = :danger_level, updated_at = :updated_at WHERE id = :id';
-    // $db = new Database();
-    // $stmt = $db->pdo->prepare($query);
-    // $stmt->bindParam(':name', $parsedBody['name']);
-    // $stmt->bindParam(':type', $parsedBody['type']);
-    // $stmt->bindParam(':habitat', $parsedBody['habitat']);
-    // $stmt->bindParam(':danger_level', $parsedBody['danger_level']);
-    // $stmt->bindParam(':updated_at', $parsedBody['updated_at']);
-    // $stmt->bindParam(':id', $id);
-    // if($stmt->execute()) {
-    //   return $response->withStatus(204);
-    // } else {
-    //   return $response->withStatus(500);
-    // }
-    return $response->withStatus(200);
+    $this->creatureRepository->update($id, $parsedBody);;
+    $body = $response->getBody();
+    $messageResponse = [ 'message' => 'Creature updated' ];
+    $body->write(json_encode($response));
+    return $response->withStatus(HttpStatusCodes::OK)->withHeader('Content-Type', 'application/json')->withBody($messageResponse);
   }
 
   // public function deleteCreature(Request $request, Response $response): Response
